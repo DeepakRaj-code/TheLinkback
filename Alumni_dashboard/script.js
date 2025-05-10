6e2cff146467e7177ee42bac4ea55d9a8afaed5d
DeepakRaj-code <deepakrajbitsindri2024@gmail.com>
2025-05-10 08:28:55 +0530

    Add files via upload

---
+++ Alumni_dashboard/script.js
@@ -0,0 +1,29 @@
+const routes = {
+    home: 'home1.html',
+    forum: 'forum.html',
+    profile: 'profile.html',
+    event: 'event.html',
+    framework: 'framework.html',
+    networks: 'networks.html',
+  };
+  
+  function navigate(pageName) {
+    location.hash = pageName; 
+  }
+  
+  function loadPage() {
+    const hash = location.hash.slice(1); 
+    const page = routes[hash] || routes['home']; 
+    fetch(page)
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById('content').innerHTML = data;
+      })
+      .catch(() => {
+        document.getElementById('content').innerHTML = '<h2>😢 Page Not Found</h2>';
+      });
+  }
+  
+  window.addEventListener('hashchange', loadPage);
+  window.addEventListener('load', loadPage);
+  
